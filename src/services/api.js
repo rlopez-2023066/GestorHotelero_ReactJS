84b1c9ef3b4eebceace2c15bebed2142f04b2c7d
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-05-22 08:54:17 -0600

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,23 @@
 import axios from "axios";
 
-//Configuración básica (General o genérica)
 const apiClient = axios.create(
     {
-        baseURL: 'http://localhost:2002/v1/auth',
+        baseURL: 'http://localhost:2002',
         timeout: 2000
     }
 )
 
-//Ruta para registrar
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export const registerRequest = async(user)=> {
     try{
-        return await apiClient.post('/register', user, {
+        return await apiClient.get('/register', user, {
             type: 'multipart/form-data'
         })
     }catch(error){
@@ -22,11 +28,10 @@ export const registerRequest = async(user)=> {
     }
 }
 
-export const loginRequest = async(user)=>{
+export const loginRequest = async(userData)=>{
     try{
-        return await apiClient.post('/login', user,{
-            type: 'multipart/form-data'
-        })
+        return await apiClient.post('/login', userData)
+        
     }catch(error){
         return {
             error: true,
@@ -34,3 +39,74 @@ export const loginRequest = async(user)=>{
         }
     }
 }
+
+export const getHotelsRequest = async () => {
+    const token = localStorage.getItem('token');
+    if (!token) throw new Error('Token no encontrado, por favor inicia sesión');
+
+    const response = await apiClient.get('/v1/hotel/getHotels', {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    return response;
+};
+
+export const addHotelRequest = async (hotelData) => {
+  const token = localStorage.getItem('token');
+  if (!token) throw new Error('Token no encontrado, por favor inicia sesión');
+
+  try {
+    const response = await apiClient.post('/v1/hotel/addHotel', hotelData, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return response;
+  } catch (error) {
+    return {
+      error: true,
+      error
+    };
+  }
+};
+
+export const deleteHotelRequest = async (hotelId) => {
+  const token = localStorage.getItem('token');
+  if (!token) throw new Error('Token no encontrado, por favor inicia sesión');
+
+  try {
+    const response = await apiClient.delete(`/v1/hotel/deleteHotel/${hotelId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return response;
+  } catch (error) {
+    return { error: true, error };
+  }
+};
+
+export const updateHotelRequest = async (hotelId, data) => {
+  const token = localStorage.getItem('token');
+  if (!token) throw new Error('Token no encontrado, por favor inicia sesión');
+
+  try {
+    const response = await apiClient.put(
+      `/v1/hotel/updateHotel/${hotelId}`,
+      data, 
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    );
+    return response;
+  } catch (error) {
+    return {
+      error: true,
+      error
+    };
+  }
+};
+
