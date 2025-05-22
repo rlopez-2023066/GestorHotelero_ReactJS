3cff7bfa452bdd92e7b504cf5024ed06f721210f
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-05-22 08:54:29 -0600

    Update useLogin.jsx

--- src/shared/hooks/useLogin.jsx
+++ src/shared/hooks/useLogin.jsx
@@ -1,34 +1,60 @@
 import { useState } from "react";
-import toast from 'react-hot-toast'
-import { loginRequest } from "../../services/api.js";
+import toast from 'react-hot-toast';
+import { loginRequest } from "../../Services/api";
 import { useNavigate } from "react-router-dom";
 
 export const useLogin = () => {
-    const [isLoading, setIsLoading] = useState(false)
-    const navigate = useNavigate()
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
-    const login = async(username, password)=> {
-        setIsLoading(true)
-        const user = {
-          userLogin:username,
-          password
-        }
-        const response = await loginRequest(user)
-        setIsLoading(false)
-    
-        if(response.error){
-            return toast.error(
-                response?.e?.response?.data ||
-                'Error general al intentar logearse. Intenta de nuevo.'
-            )
-        }
-        const { userDetails } = response.data
-        localStorage.setItem('user', JSON.stringify(userDetails))
-        navigate('/*')
+  const login = async (username, password) => {
+    setIsLoading(true);
+    const user = {
+      userLogin: username,
+      password
+    };
+
+    const response = await loginRequest(user);
+    setIsLoading(false);
+
+    if (response.error) {
+      return toast.error(
+        response?.e?.response?.data ||
+        'Error general al intentar logearse. Intenta de nuevo.'
+      );
     }
 
-  return {
-    login, 
-    isLoading
-  }
+    const { token, loggedUser } = response.data;
+
+if (!loggedUser || !loggedUser.role) {
+  return toast.error("No se pudo identificar el rol del usuario");
 }
+
+const role = loggedUser.role;
+
+localStorage.setItem('token', token);
+localStorage.setItem('user', JSON.stringify(loggedUser));
+
+switch (role) {
+
+  case 'ADMIN':
+    navigate('/dashboardAdmin');
+    break;
+  case 'ADMIN_HOTEL':
+    navigate('/dashboardAdminHotel');
+    break;
+  case 'CLIENT':
+    navigate('/dashboardClient');
+    break;
+  default:
+    navigate('/dashboardGeneral');
+    break;
+}
+
+  };
+
+  return {
+    login,
+    isLoading
+  };
+};
