5ff39df1173bf7b0c31a2218faa3f5407e5af341
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-05-22 22:08:13 -0600

    Update useAddHotel.jsx

--- src/shared/hooks/useAddHotel.jsx
+++ src/shared/hooks/useAddHotel.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { addHotelRequest } from "../../Services/api";
+import { addHotelRequest } from "./../../services/api";
 import toast from "react-hot-toast";
 
 export const useAddHotel = (onSuccess) => {
