5423787e7873301308bb5c1965e1bd00cc3aee6e
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-05-22 22:08:48 -0600

    Update useUpdateHotel.jsx

--- src/shared/hooks/useUpdateHotel.jsx
+++ src/shared/hooks/useUpdateHotel.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { updateHotelRequest } from "../../Services/api";
+import { updateHotelRequest } from "./../../services/api";
 import toast from "react-hot-toast";
 
 export const useUpdateHotel = (onSuccess) => {
