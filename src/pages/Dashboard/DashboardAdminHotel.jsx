1efba69c00c0fe8ee05e1c8f7c6fb3c80ead7967
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-05-22 22:07:47 -0600

    Update DashboardAdminHotel.jsx

--- src/pages/Dashboard/DashboardAdminHotel.jsx
+++ src/pages/Dashboard/DashboardAdminHotel.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import NotFoundPages from '../NotFound/NotFoundPages'
 
 const DashboardAdminHotel = () => {
   return (
     <div>
-      
+      <NotFoundPages />
     </div>
   )
 }
