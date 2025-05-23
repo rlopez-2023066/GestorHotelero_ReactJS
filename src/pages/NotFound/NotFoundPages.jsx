f22f1da2caecb5926195181c184929dc6b032532
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-05-22 22:08:00 -0600

    Update NotFoundPages.jsx

--- src/pages/NotFound/NotFoundPages.jsx
+++ src/pages/NotFound/NotFoundPages.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import imgNotFound from './../../img/NotFound.svg'
+import { useNavigate } from 'react-router-dom';
+
 
 export const NotFoundPages = () => {
+
+    const navigate = useNavigate();
+    
+    const handleLogout = () => {
+    localStorage.removeItem('token'); 
+    localStorage.removeItem('user');
+    navigate('/auth'); 
+    };
+
+    
   return (
         <section className="bg-gradient-to-r from-stone-500 to-stone-700 ">
         <div className="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12 ">
@@ -11,7 +23,7 @@ export const NotFoundPages = () => {
                 <p className="mt-4 text-stone-950 ">Te perdiste 🤔, Regresar al inicio no viene mal. ✌</p>
 
                 <div className="flex items-center mt-6 gap-x-3">
-                    <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+                    <button onClick={handleLogout}  className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
                         Inicio
                     </button>
                 </div>
